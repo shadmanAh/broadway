000da624b132479f3d94ba8215b65fb43de6eea6
shadmanAh <shadman061@gmail.com>
2022-01-03 19:10:41 +0330

    first commit

---
+++ app/models/play.rb
@@ -0,0 +1,8 @@
+class Play < ApplicationRecord
+
+    belongs_to :user
+    belongs_to :category
+
+    has_attached_file :play_img, styles: { :play_index => "250x350>", :play_show "325x475>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
+end
